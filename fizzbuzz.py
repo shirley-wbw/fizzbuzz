49de627e1cab1e03f0ac63f6ebe218eb06d06fe6
shirley.wbw <shirley.wbw@hotmail.com>
2018-05-01 21:50:02 +0800

    Fix output GitHub

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,14 +2,14 @@ class Fizzbuzz():
     def __init__(self,number):
         self.number=number
 #    def calculate(number):
-        if self.number%3==0 and self.number%5==0:
+        if '7' in str(self.number):
+            print('GitHub')
+        elif self.number%3==0 and self.number%5==0:
             print("fizzbuzz")
         elif self.number%3==0:
             print('fizz')
         elif self.number%5==0:
             print('buzz')
-            if '7' in str(self.number):
-                print('GitHub')
         else:
             print(self.number)
 
